9291cab7a2dde41be08276c17ce1425f5d317d08
Mohamed Abuelanin <mabuelanin@gmail.com>
2023-01-12 23:58:48 +0200

    Update run-notebook.sh

--- run-notebook.sh
+++ run-notebook.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
-echo -e "starting Jupyter Lab... please wait!"
-jupyter-lab --no-browser --ip "*" --notebook-dir ~/workshop_12Jan_2023/SV_calling_LR/ >& jupyter-session.out &
-sleep 10
+echo -e "starting Jupyter Lab... please wait!\n"
+jupyter-lab --no-browser --ip "*" --notebook-dir ~/ >& jupyter-session.out &
+sleep 20
 
 
 node_id=$(hostname)
 port=$(sed -n 's/.*localhost:\([0-9]*\)\/lab.*/\1/p' jupyter-session.out)
 
-echo "command to use on your local machine:"
-echo -e "ssh -L ${port}:${node_id}:${port} $USER@farm.cse.ucdavis.edu"
+echo -e "1) copy-paste the following command in your local terminal\n"
 
-echo -e "and then use your web browser to open http://localhost:${port}"
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+echo -e "${GREEN}ssh -L ${port}:${node_id}:${port} $USER@farm.cse.ucdavis.edu${NOCOLOR}\n"
+echo -e "2) In your internet browser, open the following URL ${GREEN}http://localhost:${port}"
